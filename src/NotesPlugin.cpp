b2820aaa069830aed52a58df06a5f50119147450
Cary Clark <caryclark@skia.org>
2018-02-21 22:32:58 -0500

    latest

--- src/NotesPlugin.cpp
+++ src/NotesPlugin.cpp
@@ -4,32 +4,105 @@ using std::vector;
 
 struct NotesModule : Module {
 	enum ParamIds {
-		PITCH_PARAM,
-        DURATION_PARAM,
-        SELECT_PARAM,
+		VERT_WHEEL,
+        HORZ_WHEEL,
+
+        RESET_BUTTON,
+        TIE_BUTTON,
+        ARP_BUTTON,
+        GLISS_BUTTON,
+        SELECT_BUTTON,
+        NOTE_BUTTON,
+
+        FUNC_BUTTON,
+        TIME_BUTTON,
+        CODA_BUTTON,
+        THREE_BUTTON,
+        PART_BUTTON,
+        REST_BUTTON,
+
 		NUM_PARAMS
 	};
+
 	enum InputIds {
 		CLOCK_INPUT,
         RESET_INPUT,
         V_OCT_INPUT,
+        REC_IN_INPUT,
+        REPEAT_INPUT,
+        PROG_INPUT,
 		NUM_INPUTS
 	};
+
 	enum OutputIds {
 		CV_1_OUTPUT,
+        CV_2_OUTPUT,
+        CV_3_OUTPUT,
+        CV_4_OUTPUT,
         GATE_1_OUTPUT,
+        GATE_2_OUTPUT,
+        GATE_3_OUTPUT,
+        GATE_4_OUTPUT,
         NUM_OUTPUTS
 	};
+
 	enum LightIds {
-		NUM_LIGHTS
+		NUM_LIGHTS      // no lights
 	};
+
+    enum class Horz_Mode {
+        Select,         // horz wheel chooses note
+        Note,           // horz wheel changes note duration
+        Extend,         // horz wheel extends selection
+        Arp,            //            selects key
+        Time,           //             selects time signature / beats per measure
+    };
+
+    enum class Button_Mode {
+        Normal,         // buttons perform primary function
+        Func,           // buttons perform secondary function
+        Part,           // buttons choose # of parts or cancel
+    };
+
+    struct Effects {
+        int notesIndex = 0;
+        int partIndex = 0;
+        int beatsPerMeasure = 0;  // zero is ignored
+        bool measureStart = false;
+        bool threeStart = false;       
+        bool three = false;       // if set, duration is equal to others in three run
+        bool threeEnd = false;       
+        bool tieStart = false;
+        bool tied = false;        // if set, nothing sent to gate out
+        bool tieEnd = false;
+        bool glissStart = false;
+        bool gliss = false;       // if set, interp cv out
+        bool glissEnd = false;
+        bool arpStart = false;
+        bool arp = false;         // if set, not independently selectable
+        bool arpEnd = false;
+        bool codaStart = false;
+        bool codaEnd = false;
+    };
+
     const float CLOCK_UP_THRESHOLD = 3;
     const float CLOCK_DOWN_THRESHOLD = 1;
 
-	vector<float> notes;
-    vector<float> durations;
+    struct Note {
+        float pitch;
+        float duration;
+        int effectIndex;
+    };
+
+	vector<Note> notes[8];        // zero value indicates rest
+    vector<Effects> effects;
+
     int currentIndex = 0;
+    int currentPart = 0;
+    int numberParts = 1;
     int editIndex = 0;
+    Horz_Mode horzMode = Horz_Mode::Select;
+    Button_Mode buttonMode = Button_Mode::Normal;
     bool clockDown = false;
 
     NotesModule() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
@@ -41,6 +114,18 @@ struct NotesModule : Module {
 	// - onReset, onRandomize, onCreate, onDelete: implements special behavior when user clicks these from the context menu
 };
 
+/* wheels have a pair of slits between an always on LED and a pair of photo diodes
+  the slits are arranged
+     0 1
+     1 0
+     1 1
+     0 1
+     etc
+  so that cw and ccw are separately detectable
+  a reading of 0 0 means a malfunction?
+
+  software emulation lets values wrap e.g. modulo
+*/
 
 void NotesModule::step() {
 	float pitch = params[PITCH_PARAM].value;
